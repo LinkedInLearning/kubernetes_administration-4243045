171544940667de3b2cbae2a5a2c262b6ee190459
Samir LAKHDARI <samir.lakhdari@mac.com>
2025-04-01 11:16:32 +0200

    Initial commit

---
+++ 4243045_fr_FR_04_04/install-etcdctl.sh
@@ -0,0 +1,7 @@
+ETCD_VER=v3.5.19
+GOOGLE_URL=https://storage.googleapis.com/etcd
+GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
+DOWNLOAD_URL=${GOOGLE_URL}
+
+curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /usr/bin/ --strip-components=1
---
+++ 4243045_fr_FR_05_01/clean.sh
@@ -0,0 +1,4 @@
+kubectl delete cm nginx-conf 
+kubectl label no host13.formation.local disktype-
+kubectl delete pod --all
+kubectl delete svc web
